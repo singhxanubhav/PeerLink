8914efee37c8a6962510fdfe0440f97851eb9b88
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-08-14 16:05:07 +0530

    project init

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,20 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import { connectDB } from "./models/index.js";
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 dotenv.config();
 
-app.get('/', (req, res)=>{
-    res.send("Hi There!")
-})
+app.get("/", (req, res) => {
+  res.send("Hi There!");
+});
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    connectDB()
-    
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+});
 
 export default app;
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_CONNECTION_STRING)
-        console.log(`MongoDB connect Successfully `);
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log(`MongoDB connect Successfully`);
+  } catch (error) {
+    console.log(error);
+  }
+};
