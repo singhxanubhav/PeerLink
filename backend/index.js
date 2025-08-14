f9be60954b3e677e0b64010cd54583b5c65eb19c
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-08-14 15:38:17 +0530

    DB connect

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./models/index.js";
+const app = express()
+
+app.use(express.json())
+dotenv.config();
+
+app.get('/', (req, res)=>{
+    res.send("Hi There!")
+})
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB()
+    
+})
+
+export default app;
